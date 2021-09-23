458d7f60f2bef0452a16d3c41115dd3c83dbd752
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2021-09-22 19:45:19 -0600

    Update jsonDeCookifier.js
    Removed extraneous commented 'console.log()' lines that were for testing

--- jsonDeCookifier.js
+++ jsonDeCookifier.js
@@ -1,16 +1,14 @@
 function jsonDeCookifier(name){
 
     let cookies = document.cookie;
-    //console.log(name)
+    
     if(cookies.length != 0){
 
         let cookieArray = cookies.split("=");
-        //console.log(cookieArray);
+        
         for(var i = 0; i < cookieArray.length - 1; i += 2){
 
         let cookieName = cookieArray[i];
-        //console.log(i);
-        //console.log(cookieName);
 
             if(cookieName = name){
 
@@ -28,12 +26,4 @@ function jsonDeCookifier(name){
 
 console.log(jsonDeCookifier('name_of_cookie_goes_here'));
     
-
-    //console.log(cookieArray);
-    //console.log(uglyCookie);
-/*    console.log(lessUglyCookie);
-    console.log(prettyCookie);
-    console.log("here's your JSON: " + yoJson);
-    console.log(JSON.parse(prettyCookie))*/
-
 
